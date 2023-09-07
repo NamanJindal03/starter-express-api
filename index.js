9e91c6d531765052d9c939a7f4dadce27b86d7c9
Naman Jindal <namanjindal113@gmail.com>
2023-09-08 02:32:44 +0530

    deploy test

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ app.all('/', (req, res) => {
     console.log("Just got a request!")
     res.send('Yo!')
 })
+app.get('/apitest', (req,res) => {
+    res.status(200).json({
+        'name': 'naman'
+    })
+})
 app.listen(process.env.PORT || 3000)
